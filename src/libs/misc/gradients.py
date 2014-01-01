e6b624a44d2119e0079c70bf5998c8c52d0bd8d8
Christopher Harris <chris_jh@me.com>
2014-01-01 20:26:23 +0000

    removed custom method don’t  need

--- src/libs/misc/gradients.py
+++ src/libs/misc/gradients.py
@@ -263,30 +263,6 @@ def horizontal(size, startcolor, endcolor):
                       )
     return pygame.transform.scale(bigSurf, size)
     
-def horizontal2(surface, size, startcolor, endcolor):
-    """
-    Draws a horizontal linear gradient filling the entire surface. Returns a
-    surface filled with the gradient (numeric is only 2-3 times faster).
-    """
-    width = size[0]
-    bigSurf = surface.convert_alpha()
-    dd = 1.0/width
-    sr, sg, sb, sa = startcolor
-    er, eg, eb, ea = endcolor
-    rm = (er-sr)*dd
-    gm = (eg-sg)*dd
-    bm = (eb-sb)*dd
-    am = (ea-sa)*dd
-    for y in range(width):
-        bigSurf.set_at((y,0),
-                        (int(sr + rm*y),
-                         int(sg + gm*y),
-                         int(sb + bm*y),
-                         int(sa + am*y))
-                      )
-    return pygame.transform.scale(bigSurf, size)
-
-
 def radial(radius, startcolor, endcolor):
     """
     Draws a linear raidal gradient on a square sized surface and returns
